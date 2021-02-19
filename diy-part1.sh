fa8ff885d72fb62b8d8748112cda7b18e9aaed3b
limi00 <75186809+limi00@users.noreply.github.com>
2021-02-19 12:53:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,5 +22,11 @@
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/luci-app-usb3disable package/diy/luci-app-usb3disable
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lienol/luci-app-passwall package/diy/luci-app-passwall
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lienol/dns2socks package/diy/dns2socks
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lienol/luci-app-ssr-libev-server package/diy/luci-app-ssr-libev-server
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lienol/luci-app-ssr-mudb-server package/diy/luci-app-ssr-mudb-server
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lienol/luci-app-trojan-server package/diy/luci-app-trojan-server
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lienol/dns2socks package/diy/dns2socks
+
 
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
