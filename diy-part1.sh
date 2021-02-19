d9cd8c34d2c0855a2e95b8ba46812f99e07deb05
limi00 <75186809+limi00@users.noreply.github.com>
2021-02-19 12:41:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git project https://github.com/project-openwrt/openwrt' feeds.conf.default
-sed -i '$a src-git openwrt https://github.com/openwrt/packages' feeds.conf.default
+# sed -i '$a src-git openwrt https://github.com/openwrt/packages' feeds.conf.default
 
 # # Define My Package
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
