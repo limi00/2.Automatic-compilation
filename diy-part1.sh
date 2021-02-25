96f4d6806a165a451a7b7bf3b756914bf36c4849
limi00 <75186809+limi00@users.noreply.github.com>
2021-02-26 06:28:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,7 +31,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy/luci-app
 # git clone https://github.com/tty228/luci-app-serverchan.git package/mine/luci-app-serverchan
 # git clone https://github.com/linkease/ddnsto-openwrt.git package/network/services/ddnsto
 # git clone https://github.com/linkease/ddnsto-openwrt.git package/diy/luci-app-ddnsto
-svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-ddnsto package/diy/luci-app-ddnsto
+# svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-ddnsto package/diy/luci-app-ddnsto
 svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-autotimeset package/diy/luci-app-autotimeset
 svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-control-weburl package/diy/luci-app-control-weburl
 
