4ccf2bda300ba8fe3e816fab2ebd63437fe3c74c
limi00 <75186809+limi00@users.noreply.github.com>
2021-02-25 22:28:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,5 +29,10 @@ svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/luci-app-
 # git clone https://github.com/immortalwrt/openwrt-passwall.git package/diy/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy/luci-app-passwall
 # git clone https://github.com/tty228/luci-app-serverchan.git package/mine/luci-app-serverchan
+svn co https://github.com/linkease/ddnsto-openwrt/trunk/ddnsto package/network/services/ddnsto
+svn co https://github.com/linkease/ddnsto-openwrt/trunk/luci-app-ddnsto package/diy/luci-app-ddnsto
+# svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-ddnsto package/diy/luci-app-ddnsto
+svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-autotimeset package/diy/luci-app-autotimeset
+svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-control-weburl package/diy/luci-app-control-weburl
 
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
